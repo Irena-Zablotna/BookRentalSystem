44bdfb735c6c9f440d4726c1efae4b7c88727c22
Irena Zablotna <irena.zablotna@consulenti.csi.it>
2024-04-21 17:20:51 +0200

    added some methods in Book Service class

--- BookRentalSystem/Book.cs
+++ BookRentalSystem/Book.cs
@@ -8,19 +8,22 @@ namespace BookRentalSystem
 {
  public class Book
     {
+        public int Id { get; set; }
         public string Author { get; }
         public string Title { get; }
         public string Category { get; }
         public bool IsAvailable { get; set; }
         public int? Opinion { get; set; }
         public List<int> Opinions { get; } 
-        public List <User> users { get; }
-        public Book(string author, string title, string category)
+        public List <User> Users { get; }
+        public Book(int id, string author, string title, string category)
         {
             Author = author;
             Title = title;
             Category = category;
             IsAvailable = true;
+            Opinions = new List<int>();
+            Users = new List<User>();
         }
     }
 }
--- BookRentalSystem/BookService.cs
+++ BookRentalSystem/BookService.cs
@@ -8,5 +8,78 @@ namespace BookRentalSystem
 {
     public class BookService
     {
+        private List<Book> books = new List<Book>();
+
+        public void InitializeBooks()
+        {
+            books.Add(new Book(1,"Author1", "Title1", "Category1" ));
+            books.Add(new Book(2,"Author2", "Title2", "Category2" ));
+            books.Add(new Book(3,"Author3", "Title3", "Category1"));
+            books.Add(new Book(4,"Author4", "Title4", "Category3"));
+        }
+
+        public void AddBook()
+        {
+            Console.Write("Insert the author of the book: ");
+            string author = Console.ReadLine();
+            Console.Write("Insert the title of the book: ");
+            string title = Console.ReadLine();
+            Console.Write("Insert the category of the book: ");
+            string category = Console.ReadLine();
+            int id = books.Count + 1;
+
+            books.Add(new Book(id, author, title, category));
+            Console.WriteLine("Book added succesfully.");
+        }
+
+
+        public void RemoveBook(int id)
+        {
+            // Implementazione...
+        }
+
+        public void SearchBookByAuthor()
+        {
+            // Implementazione...
+        }
+
+        public void SearchBookByCategory()
+        {
+            // Implementazione...
+        }
+
+        public void SearchBookByTitle()
+        {
+            // Implementazione...
+        }
+
+        public void DisplayBookStatus()
+        {
+            // Implementazione...
+        }
+
+        public void RentBook()
+        {
+            // Implementazione...
+        }
+
+        public void WriteBookReview()
+        {
+            // Implementazione...
+        }
+
+        public void ReadBookReviews()
+        {
+            // Implementazione...
+        }
+
+      
+
+
+        public void DisplayStatistics()
+        {
+            // Implementazione...
+        }
     }
 }
+
