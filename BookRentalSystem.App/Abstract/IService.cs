9b9c4812aa53dfcc7b18444a1ac5686fc1b9cd90
Irene Zablotna <irene.zablotna@hotmail.it>
2024-06-02 23:06:11 +0200

    Started refactoring, created a new branch

---
+++ BookRentalSystem.App/Abstract/IService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRentalSystem.App.Abstract
+{
+    public interface IService <T>   
+    {
+        List <T> Items { get; set; }
+
+        List <T> GetAll ();
+
+         T getItemById (int id); 
+
+        int AddItem(T item);
+
+        int UpgradeItem(T item);
+
+        void RemoveItem (T item);
+
+
+    }
+}
---
+++ BookRentalSystem.App/Common/BaseService.cs
@@ -0,0 +1,56 @@
+﻿using BookRentalSystem.App.Abstract;
+using BookRentalSystem.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRentalSystem.App.Common
+{
+    public class BaseService<T> : IService<T> where T : BaseEntity
+    {
+        private object item;
+
+        public List<T> Items { get ; set; }
+        public BaseService() { 
+            Items = new List<T>();
+        }
+
+        public List<T> GetAll()
+        {
+            return Items;
+        }
+
+        public T getItemById(int id)
+        {
+            var entity = Items.FirstOrDefault(p => p.Id == id);
+            return entity;
+        }
+
+        public int AddItem(T item)
+        {
+            Items.Add(item);
+            return item.Id;
+        }
+
+        public int UpgradeItem(T item)
+        {
+            var entity = Items.FirstOrDefault(p=>p.Id == item.Id);
+            if(entity != null)
+            {
+                entity = item;
+            }
+            return item.Id;
+        }
+
+        public void RemoveItem(T item)
+        {
+            Items.Remove(item);
+        }
+    }
+
+    }
+
+        
+    
